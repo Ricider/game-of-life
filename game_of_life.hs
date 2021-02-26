b4a1c7e445c8d37b158337e776983d79017fbc22
Sarp <54453420+Ricider@users.noreply.github.com>
2021-02-26 14:30:42 +0300

    changed empty space

--- game_of_life.hs
+++ game_of_life.hs
@@ -2,8 +2,8 @@ import Data.Array
 import Control.Monad
 import System.Environment
 
-dim_x=100
-dim_y=50
+dim_x=70
+dim_y=40
 
 draw_board board = putStr board_str where
 	board_str = concat [(board ! i) ++ (if i `mod` dim_x /= (dim_x-1) then "" else "\n") | i <- [0..dim_x*dim_y-1]]
@@ -11,10 +11,10 @@ draw_board board = putStr board_str where
 step_board board = board//[(n, new_value n) | n <- [0..dim_x*dim_y-1]] where
   	new_value index =   if (board!index == "O") then
 							if ((neighbors index == 3) || (neighbors index == 2)) then "O"
-							else "."
+							else " "
 						else
 							if (neighbors index == 3) then "O"
-							else "."
+							else " "
   	neighbors index = sum [1 | x <- (filter (>=0) $ filter (<(dim_x*dim_y)) $ map ($index) indexes), board!x == "O"]
 	indexes = [(+(-dim_x-1)),(+(-dim_x)),(+(-dim_x+1)),(+(-1)),(+1),(+(dim_x-1)),(+dim_x),(+(1+dim_x))]
 
@@ -29,7 +29,6 @@ step_sim board ttl = do
 main :: IO ()
 main = do
 	duration:rest <- getArgs
-	let empty = array (0,(dim_x*dim_y-1)) [(i,".") | i <- [0..dim_x*dim_y-1]]
+	let empty = array (0,(dim_x*dim_y-1)) [(i," ") | i <- [0..dim_x*dim_y-1]]
 	let start = empty//[(i,"O") | i <- [3,3+dim_x,3+2*dim_x,2+2*dim_x,1+dim_x]]
 	step_sim start $ read duration
- 
