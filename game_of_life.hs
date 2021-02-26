268761e756b969a37e22153d5f64ad8590c4047e
Sarp <54453420+Ricider@users.noreply.github.com>
2021-02-26 14:07:06 +0300

    synchronised screen

--- game_of_life.hs
+++ game_of_life.hs
@@ -2,24 +2,25 @@ import Data.Array
 import Control.Monad
 import System.Environment
 
-dim=50
+dim_x=100
+dim_y=50
 
 draw_board board = putStr board_str where
-	board_str = concat [(board ! i) ++ (if i `mod` dim /= (dim-1) then "" else "\n") | i <- [0..dim*dim-1]]
+	board_str = concat [(board ! i) ++ (if i `mod` dim_x /= (dim_x-1) then "" else "\n") | i <- [0..dim_x*dim_y-1]]
 
-step_board board = board//[(n, new_value n) | n <- [0..dim*dim-1]] where
+step_board board = board//[(n, new_value n) | n <- [0..dim_x*dim_y-1]] where
   	new_value index =   if (board!index == "O") then
 							if ((neighbors index == 3) || (neighbors index == 2)) then "O"
 							else "."
 						else
 							if (neighbors index == 3) then "O"
 							else "."
-  	neighbors index = sum [1 | x <- (filter (>=0) $ filter (<(dim*dim)) $ map ($index) indexes), board!x == "O"]
-	indexes = [(+(-dim-1)),(+(-dim)),(+(-dim+1)),(+(-1)),(+1),(+(dim-1)),(+dim),(+(1+dim))]
+  	neighbors index = sum [1 | x <- (filter (>=0) $ filter (<(dim_x*dim_y)) $ map ($index) indexes), board!x == "O"]
+	indexes = [(+(-dim_x-1)),(+(-dim_x)),(+(-dim_x+1)),(+(-1)),(+1),(+(dim_x-1)),(+dim_x),(+(1+dim_x))]
 
 step_sim board ttl = do
 	draw_board board
-	putStrLn ""
+	putStr "\x1b[H"
 	if ttl == 0 then
 		putStrLn "Done !"
 	else
@@ -28,7 +29,7 @@ step_sim board ttl = do
 main :: IO ()
 main = do
 	duration:rest <- getArgs
-	let empty = array (0,(dim*dim-1)) [(i,".") | i <- [0..dim*dim-1]]
-	let start = empty//[(i,"O") | i <- [3,53,103,102,51]]
+	let empty = array (0,(dim_x*dim_y-1)) [(i,".") | i <- [0..dim_x*dim_y-1]]
+	let start = empty//[(i,"O") | i <- [3,3+dim_x,3+2*dim_x,2+2*dim_x,1+dim_x]]
 	step_sim start $ read duration
  
